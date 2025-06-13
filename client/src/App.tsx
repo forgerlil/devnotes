80325774e17f95286e08028dc1e53752ba4e744b
L Forger <lilianforger@gmail.com>
2025-06-13 20:16:45 +0200

    polishing configs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-	return (
-		<>
-			<h1 className='text-3xl font-bold'>Hello World</h1>
-		</>
-	);
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>Hello World</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
