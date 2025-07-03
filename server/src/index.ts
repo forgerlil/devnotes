2f0a636da98040aafc1f494357632182e37004b6
L Forger <lilianforger@gmail.com>
2025-07-03 11:58:28 +0200

    Setup for server testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express'
+import express, { Application } from 'express'
 import cors from 'cors'
 import { errorHandler } from './middlewares/errorHandler.js'
 import configs from './configs/index.js'
 import routes from './routes/index.js'
 
-const app = express()
+const app: Application = express()
 
 app.use(express.json())
 app.use(
@@ -17,3 +17,5 @@ app.use('/api', routes)
 app.use(errorHandler)
 
 app.listen(configs.port, () => console.log(`Server up on port ${configs.port}`))
+
+export default app
