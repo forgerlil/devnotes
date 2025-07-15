fe7575ec12e9f28feb5e14969014b9e3978aa2e9
L Forger <lilianforger@gmail.com>
2025-07-15 19:32:00 +0200

    Added tests for Login page

--- client/__tests__/Login.test.tsx
+++ client/__tests__/Login.test.tsx
@@ -1,13 +1,105 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
-import Login from '@/pages/Login'
+import { createRoutesStub } from 'react-router'
+import { Login, Register, NoteDashboard } from '@/pages'
+import { toastError, toastSuccess } from '@/lib/toastify'
 
-describe('Login page', () => {
-  beforeEach(() => {
-    render(<Login />)
+const loginRoute = {
+  path: '/login',
+  Component: Login,
+  action: () => {},
+}
+
+const registerRoute = {
+  path: '/register',
+  Component: Register,
+  action: () => {},
+}
+
+const notesRoute = {
+  path: '/notes/:id',
+  Component: NoteDashboard,
+  action: () => {},
+}
+
+vi.mock('@/lib/toastify', () => ({
+  toastError: vi.fn(),
+  toastSuccess: vi.fn(),
+}))
+
+describe('<Login />', () => {
+  afterEach(() => {
+    loginRoute.action = () => {}
   })
 
   it('should render the Login page', () => {
+    const Stub = createRoutesStub([loginRoute])
+
+    render(<Stub initialEntries={['/login']} />)
+
     expect(screen.getByRole('heading', { level: 1, name: 'Login' })).toBeInTheDocument()
+  })
+
+  it('should show error toast if form is submitted with empty fields', async () => {
+    loginRoute.action = () => ({
+      error: 'Email and password are required',
+      data: null,
+    })
+    const Stub = createRoutesStub([loginRoute])
+
+    render(<Stub initialEntries={['/login']} />)
+    await userEvent.click(screen.getByRole('button', { name: 'Login' }))
+
+    expect(toastError).toHaveBeenCalledWith('Email and password are required')
+    expect(toastSuccess).not.toHaveBeenCalled()
+  })
+
+  it('shows error toast if fields fail validation', async () => {
+    loginRoute.action = () => ({
+      error: 'Incorrect username or password, please verify and try again',
+      data: null,
+    })
+    const Stub = createRoutesStub([loginRoute])
+
+    render(<Stub initialEntries={['/login']} />)
+    await userEvent.type(screen.getByPlaceholderText('Email'), 'test@test.com')
+    await userEvent.type(screen.getByPlaceholderText('Password'), 'password')
+    await userEvent.click(screen.getByRole('button', { name: 'Login' }))
+
+    expect(toastError).toHaveBeenCalledWith(
+      'Incorrect username or password, please verify and try again'
+    )
+    expect(toastSuccess).not.toHaveBeenCalled()
+  })
+
+  it('shows success toast and redirects to notes page with valid credentials', async () => {
+    loginRoute.action = () => ({
+      error: null,
+      data: null,
+    })
+
+    const Stub = createRoutesStub([loginRoute, notesRoute])
+
+    render(<Stub initialEntries={['/login']} />)
+    await userEvent.type(screen.getByPlaceholderText('Email'), 'test@test.com')
+    await userEvent.type(screen.getByPlaceholderText('Password'), 'Password123!')
+    await userEvent.click(screen.getByRole('button', { name: 'Login' }))
+
+    await waitFor(() => {
+      expect(toastSuccess).toHaveBeenCalledWith('Welcome back!')
+    })
+
+    expect(await screen.findByRole('heading', { name: 'NoteDashboard' })).toBeInTheDocument()
+  })
+
+  it('redirects to register page on respective link', async () => {
+    const Stub = createRoutesStub([loginRoute, registerRoute])
+
+    render(<Stub initialEntries={['/login']} />)
+    await userEvent.click(screen.getByRole('link', { name: 'Register' }))
+
+    expect(await screen.findByRole('heading', { name: 'Register' })).toBeInTheDocument()
+    expect(screen.queryByRole('heading', { name: 'Login' })).not.toBeInTheDocument()
   })
 })
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,17 +1,10 @@
-import { useEffect, useState } from 'react'
-import { Link, Form, useActionData, useNavigate } from 'react-router'
-import { TiMail } from 'react-icons/ti'
-import { IoKeyOutline } from 'react-icons/io5'
-import { FaRegEyeSlash, FaRegEye } from 'react-icons/fa'
+import { useEffect } from 'react'
+import { useActionData, useNavigate } from 'react-router'
+import { LoginForm } from '@/components/auth'
 import { toastError, toastSuccess } from '@/lib/toastify'
 import { LoginResponse } from '@/types/auth.types'
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  })
-  const [showPassword, setShowPassword] = useState(false)
   const actionData = useActionData<LoginResponse>()
   const navigate = useNavigate()
 
@@ -25,73 +18,7 @@ const Login = () => {
     }
   }, [actionData, navigate])
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setFormData({ ...formData, [name]: value })
-  }
-
-  return (
-    <main className='flex justify-center items-center h-screen relative bg-neutral'>
-      <img
-        src='/photo-1517842645767-c639042777db.avif'
-        alt='logo'
-        className='w-full h-full object-cover absolute top-0 left-0 blur-lg'
-        draggable={false}
-      />
-      <div className='card w-[448px] shadow-sm bg-base-300/85'>
-        <div className='card-body'>
-          <h1 className='text-3xl font-thin tracking-wide text-center mb-12'>Login</h1>
-          <Form className='flex flex-col gap-4' method='post'>
-            <label className='input input-lg w-full validator'>
-              <TiMail size={24} className='opacity-50' />
-              <input
-                type='email'
-                name='email'
-                placeholder='Email'
-                value={formData.email}
-                onChange={handleChange}
-              />
-            </label>
-            <div className='validator-hint hidden -mt-2'>Enter valid email address</div>
-            <div className='relative flex items-center flex-wrap gap-2'>
-              <label className='input input-lg w-full'>
-                <IoKeyOutline size={20} className='opacity-50' />
-                <input
-                  type={showPassword ? 'text' : 'password'}
-                  name='password'
-                  placeholder='Password'
-                  value={formData.password}
-                  onChange={handleChange}
-                />
-              </label>
-              {showPassword ? (
-                <FaRegEyeSlash
-                  size={20}
-                  className='w-fit h-[90%] absolute z-10 p-3 right-1 bg-base-100 text-gray-400 hover:cursor-pointer'
-                  onClick={() => setShowPassword((prev) => !prev)}
-                />
-              ) : (
-                <FaRegEye
-                  size={20}
-                  className='w-fit h-[90%] absolute z-10 p-3 right-1 bg-base-100 text-gray-400 hover:cursor-pointer'
-                  onClick={() => setShowPassword((prev) => !prev)}
-                />
-              )}
-            </div>
-            <button className='btn btn-primary btn-block my-10' type='submit'>
-              Login
-            </button>
-          </Form>
-          <p className='text-center text-sm text-base-content font-thin'>
-            Don't have an account?{' '}
-            <Link to='/register' className='link link-primary link-hover font-bold'>
-              Register
-            </Link>
-          </p>
-        </div>
-      </div>
-    </main>
-  )
+  return <LoginForm />
 }
 
 export default Login
